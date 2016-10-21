a72b57a1611a2ebf8d6adadd296793edf7ea4d8d
JackVeromeev <veromeev1989@gmail.com>
2016-10-21 08:22:21 +0300

    Add diagrams(Lab3)

--- app/src/main/java/com/jackveromeev/ejournal/MainActivity.java
+++ app/src/main/java/com/jackveromeev/ejournal/MainActivity.java
@@ -2,6 +2,9 @@ package com.jackveromeev.ejournal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button myFirstButton=(Button)findViewById(R.id.button);
+        final TextView myText=(TextView)findViewById(R.id.helloworld);
+        myFirstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myText.setText("fuck android!".toCharArray(),0,13);
+            }
+        });
     }
 }
